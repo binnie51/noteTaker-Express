4421e986789b5e0d3bf38d1096c9e64757034fc8
Vincent Tjia <vtjia51@gmail.com>
2022-05-20 23:56:16 -0400

    add GET for homepage and notes page

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+// Dependencies
 const express = require('express');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -17,9 +17,9 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+// GET Route for notes.html page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
 app.listen(PORT, () =>
