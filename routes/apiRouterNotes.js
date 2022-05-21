f3890c4b36b56ad55825c67bea826d00040ebcb0
Vincent Tjia <vtjia51@gmail.com>
2022-05-21 01:13:00 -0400

    import packages and dependencies into apiRouterNotes.js

---
+++ routes/apiRouterNotes.js
@@ -0,0 +1,9 @@
+// packages and dependencies
+const express = require('express');
+const notesRoute = require('express').Router()
+const path = require('path')
+const fs = require('fs')
+
+
+
+module.exports = notesRoute
