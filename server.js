780f2e2a51c68a1e1241785f117a3f08465f0421
Vincent Tjia <vtjia51@gmail.com>
2022-05-23 15:29:42 -0400

    fixed path

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 // Dependencies
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index');
-
+const fs = require('fs');
+const { v4: uuidv4} = require('uuid');
+const database = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET routes 
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -24,9 +24,48 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET route to read json file and return all saved notes as json
+app.get('/api/notes', (req, res) => res.json(database));
+
 // GET wildcard, fallback route for when a user attempts to visit routes that don't exist
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+// POST route, receive a new note to save on the request body
+app.post('/api/notes', (req, res) => {
+  // Destructuring the notes in req.body
+  const { title, text } = req.body;
+
+  // if every required properties are present,
+  // then variable for the object will be saved
+  if (title && text) {
+      const newNote = {
+          title,
+          text,
+          id: uuidv4(),
+      };
+      
+      // convert str into JSON object
+      database.push(newNote);
+
+      // Write to ile
+      fs.writeFile(`./db/db.json`, JSON.stringify(database), (err) =>
+          err
+              ? console.error(err)
+              : console.log('Successfully updated reviews!')
+      );
+          
+      const response = {
+          status: 'success',
+          body: newNote,
+      };
+
+      console.log(response);
+      res.json(response);
+  } else {
+      res.status(400).json('Update notes error!')
+  }
 });
 
 // Listen for connections
